b86b534e75ff76bcd4b79b0ebd526f46e6c230a6
DaniCal <daniel.lenerd.lohse@gmail.com>
2015-04-23 17:11:56 +0200

    Table context moved to MainPage

--- TimeTracker/MainPage.xaml.cs
+++ TimeTracker/MainPage.xaml.cs
@@ -100,17 +100,17 @@ namespace TimeTracker
                 textBoxTime.Text = "" + getMinutes(totalSeconds) + ":" + getSeconds(totalSeconds);
                 buttonStartRecording.Content = "Start recording";
                 createNewSessionItem("projectX", currentTimestampStart, currentTimestampStop);
-                saveChangesToDatabase();
-                testQueryDatabase();
             }
         }
 
         private void saveData_Click(object sender, RoutedEventArgs e)
         {
+            saveChangesToDatabase();
         }
 
-        private void startRecording_Click(object sender, RoutedEventArgs e)
+        private void queryData_Click(object sender, RoutedEventArgs e)
         {
+            testQueryDatabase();
         }
 
 
@@ -158,6 +158,8 @@ namespace TimeTracker
         public void createNewSessionItem(string projectId, int timestampStart, int timestampStop)
         {
             SessionItem newSession = new SessionItem { ProjectId = projectId, TimestampStart = timestampStart, TimestampStop = timestampStop};
+            SessionItems.Add(newSession);
+            //sessionDB.GetTable<SessionItem>().Attach(newSession, true);
             sessionDB.SessionItems.InsertOnSubmit(newSession);
         }
 
@@ -168,12 +170,14 @@ namespace TimeTracker
 
         private void testQueryDatabase()
         {
-            var sessionItemsInDB = from SessionItem todo in sessionDB.SessionItems select todo;
-            SessionItems = new ObservableCollection<SessionItem>(sessionItemsInDB);
-            foreach (var item in SessionItems)
-            {
-                Debug.WriteLine(item.TimestampStart);
-            }
+            //var sessionItemsInDB = from SessionItem todo in sessionDB.SessionItems select todo;
+            //SessionItems = new ObservableCollection<SessionItem>(sessionItemsInDB);
+            //foreach (var item in SessionItems)
+            //{
+            //    Debug.WriteLine(item.TimestampStart);
+            //}
+
+            Debug.WriteLine(sessionDB.GetTable<SessionItem>().Count());
 
         }
     }
@@ -187,5 +191,117 @@ namespace TimeTracker
         { }
 
         public Table<SessionItem> SessionItems;
+    }
+
+    [Table]
+    public class SessionItem : INotifyPropertyChanged, INotifyPropertyChanging
+    {
+        private int _sessionItemId;
+
+        [Column(IsPrimaryKey = true, IsDbGenerated = true, DbType = "INT NOT NULL Identify", CanBeNull = false, AutoSync = AutoSync.OnInsert)]
+        public int SessionItemId
+        {
+            get
+            {
+                return _sessionItemId;
+            }
+            set
+            {
+                if (_sessionItemId != value)
+                {
+                    NotifyPropertyChanging("SessionItemId");
+                    _sessionItemId = value;
+                    NotifyPropertyChanged("SessionItemId");
+                }
+            }
+        }
+
+
+
+        private int _timestampStart;
+        [Column]
+        public int TimestampStart
+        {
+            get
+            {
+                return _timestampStart;
+            }
+            set
+            {
+                if (_timestampStart != value)
+                {
+                    NotifyPropertyChanging("TimestampStart");
+                    _timestampStart = value;
+                    NotifyPropertyChanged("TimestampStart");
+                }
+            }
+        }
+
+        private int _timestampStop;
+        [Column]
+        public int TimestampStop
+        {
+            get
+            {
+                return _timestampStop;
+            }
+            set
+            {
+                if (_timestampStop != value)
+                {
+                    NotifyPropertyChanging("TimestampStop");
+                    _timestampStop = value;
+                    NotifyPropertyChanged("TimestampStop");
+                }
+            }
+        }
+
+        private string _projectId;
+        [Column]
+        public string ProjectId
+        {
+            get
+            {
+                return _projectId;
+            }
+            set
+            {
+                if (_projectId != value)
+                {
+                    NotifyPropertyChanging("ProjectId");
+                    _projectId = value;
+                    NotifyPropertyChanged("ProjectId");
+
+                }
+            }
+        }
+
+        #region INotifyPropertyChanged Members
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanging Members
+
+        public event PropertyChangingEventHandler PropertyChanging;
+
+        private void NotifyPropertyChanging(string propertyName)
+        {
+            if (PropertyChanging != null)
+            {
+                PropertyChanging(this, new PropertyChangingEventArgs(propertyName));
+            }
+        }
+
+        #endregion
     }
 }
