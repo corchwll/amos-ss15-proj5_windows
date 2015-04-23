b200279f98c1b172c51aa683b3e1ae521bff2d99
DaniCal <daniel.lenerd.lohse@gmail.com>
2015-04-23 16:52:38 +0200

    buttons/listeners save and query added

--- TimeTracker/MainPage.xaml.cs
+++ TimeTracker/MainPage.xaml.cs
@@ -13,11 +13,12 @@ using System.Windows.Threading;
 using Microsoft.Phone.Controls;
 
 
-using System.Linq;
+
 using System.Data.Linq.Mapping;
 using System.ComponentModel;
 using System.Collections.ObjectModel;
 using System.Data.Linq;
+using System.Diagnostics;
 
 namespace TimeTracker
 {
@@ -28,11 +29,14 @@ namespace TimeTracker
         private Int32 totalSeconds;
         Boolean isTimerRunning = false;
 
+        private int currentTimestampStart;
+        private int currentTimestampStop;
+
 
         private SessionDataContext sessionDB;
 
         private ObservableCollection<SessionItem> _sessionItems;
-        public ObservableCollection<SessionItem> SessionItem
+        public ObservableCollection<SessionItem> SessionItems
         {
             get
             {
@@ -80,6 +84,7 @@ namespace TimeTracker
         {
             //if the timer was not started yet it will get started and the button content will get changed
             if(!isTimerRunning){
+                currentTimestampStart = getUnixTimestamp();
                 totalSeconds = 0;
                 textBoxTime.Text = "00:00";
                 isTimerRunning = true;
@@ -89,12 +94,25 @@ namespace TimeTracker
             //if the timer is already running, it will get stopoped, the button content will get changed and
             //the output is showen in the UI
             else{
+                currentTimestampStop = getUnixTimestamp();
                 isTimerRunning = false;
                 dispatcherTimer.Stop();
                 textBoxTime.Text = "" + getMinutes(totalSeconds) + ":" + getSeconds(totalSeconds);
                 buttonStartRecording.Content = "Start recording";
+                createNewSessionItem("projectX", currentTimestampStart, currentTimestampStop);
+                saveChangesToDatabase();
+                testQueryDatabase();
             }
         }
+
+        private void saveData_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void startRecording_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
 
         //returns the amount of minutes of a total amount of seconds as a 2 digits String
         //example 65 -> "01",  620 -> "10"
@@ -118,6 +136,11 @@ namespace TimeTracker
             return "" + result;
         }
 
+        private Int32 getUnixTimestamp()
+        {
+            return (Int32)(DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds;
+        }
+
         #region INotifyPropertyChanged Members
 
         public event PropertyChangedEventHandler PropertyChanged;
@@ -135,7 +158,23 @@ namespace TimeTracker
         public void createNewSessionItem(string projectId, int timestampStart, int timestampStop)
         {
             SessionItem newSession = new SessionItem { ProjectId = projectId, TimestampStart = timestampStart, TimestampStop = timestampStop};
-            sessionDB.sessionItems.InsertOnSubmit(newSession);
+            sessionDB.SessionItems.InsertOnSubmit(newSession);
+        }
+
+        public void saveChangesToDatabase()
+        {
+            sessionDB.SubmitChanges();
+        }
+
+        private void testQueryDatabase()
+        {
+            var sessionItemsInDB = from SessionItem todo in sessionDB.SessionItems select todo;
+            SessionItems = new ObservableCollection<SessionItem>(sessionItemsInDB);
+            foreach (var item in SessionItems)
+            {
+                Debug.WriteLine(item.TimestampStart);
+            }
+
         }
     }
 
@@ -147,6 +186,6 @@ namespace TimeTracker
         public SessionDataContext(string connectionString) : base(connectionString)
         { }
 
-        public Table<SessionItem> sessionItems;
+        public Table<SessionItem> SessionItems;
     }
 }
