307503435da16c7e1fcb6611725cd2f7adc784ea
Daniel Lohse <daniel.lenerd.lohse@gmail.com>
2015-05-29 00:54:13 +0200

    Refactoring - CreateProjectPage simplified (data passing extracted)

--- TimeTracker/Pages/CreateProjectPage.xaml.cs
+++ TimeTracker/Pages/CreateProjectPage.xaml.cs
@@ -1,17 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿/*
+ *     Mobile Time Accounting
+ *     Copyright (C) 2015
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU Affero General Public License as
+ *     published by the Free Software Foundation, either version 3 of the
+ *     License, or (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU Affero General Public License for more details.
+ *
+ *     You should have received a copy of the GNU Affero General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Navigation;
 using Microsoft.Phone.Controls;
-using Microsoft.Phone.Shell;
 
 namespace TimeTracker
 {
     public partial class CreateProjectPage : PhoneApplicationPage
     {
+        private const string NavigationBody = "/Pages/MainPivotPage.xaml?";
+        private const string LinkElement = "&";
+
         public CreateProjectPage()
         {
             InitializeComponent();
@@ -32,7 +47,35 @@ namespace TimeTracker
                 return;
             }
 
-            NavigationService.Navigate(new Uri("/Pages/MainPivotPage.xaml?projectName=" + projectName + "&" + "projectId=" + projectId + "&" + "finalDate=" + FinalDate, UriKind.Relative));
+            NavigationService.Navigate(new Uri(CreateDataUri(projectName, projectId,
+                                        FinalDate.ToString()), UriKind.Relative));
+
+        }
+
+        private string CreateDataUri(string projectName, string projectId, string date)
+        {
+            return NavigationBody
+                   + ProjectNameUri(projectName)
+                   + LinkElement
+                   + ProjectIdUri(projectId)
+                   + LinkElement
+                   + ProjectDateUri(date);
+        }
+
+        private string ProjectNameUri(string projectName)
+        {
+            return QueryDictionary.ProjectName + "=" + projectName;
+        }
+
+        private string ProjectIdUri(string projectId)
+        {
+            return QueryDictionary.ProjectId + "=" + projectId;
+
+        }
+
+        private string ProjectDateUri(string date)
+        {
+            return QueryDictionary.ProjectFinalDate + "=" + date;
 
         }
     }
