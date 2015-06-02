977271aa3c585454a4163761bece096008b019c9
Daniel Lohse <daniel.lenerd.lohse@gmail.com>
2015-06-02 10:34:25 +0200

    CalculateWorkDays added to DashboardInformation for Business Logic

--- TimeTracker/BusinessLogic/DashboardInformation.cs
+++ TimeTracker/BusinessLogic/DashboardInformation.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -36,6 +37,48 @@ namespace TimeTracker.BusinessLogic
 		}
 
 		return user.OverTime + overTimeInHours;
-	}
+	    
+     }
+        /**
+	 * This method calculates the amount of workdays that exist inbetween the given interval.
+	 *
+	 * @param start the start date of the interval
+	 * @param stop the stop date of the interval
+	 * @return the amount of workdays
+	 * methodtype helper method
+	 * pre start != null && stop != null
+	 * post correct amount of workdays calculated
+	 */
+        protected long CalculateWorkdays(DateTime start, DateTime stop)
+        {
+            
+            //backup on which weekday the intervall started
+            DayOfWeek startWeekday = start.DayOfWeek;
+            
+            //start interval on mondays
+            start.AddDays(-startWeekday.CompareTo(DayOfWeek.Monday));
+
+            
+            //backup on which weekday the intervall stopped
+            DayOfWeek stopWeekday = stop.DayOfWeek;
+            //end interval on mondays
+            stop.AddDays(-stopWeekday.CompareTo(DayOfWeek.Monday));
+
+            //calc
+            long days = (stop.Ticks - start.Ticks) / (1000 * 60 * 60 * 24);
+            long workDays = days * 5 / 7;
+
+            if (startWeekday == DayOfWeek.Sunday)
+            {
+                startWeekday = DayOfWeek.Monday;
+            }
+
+            if (stopWeekday == DayOfWeek.Sunday)
+            {
+                stopWeekday = DayOfWeek.Monday;
+            }
+
+            return workDays - startWeekday.CompareTo(DayOfWeek.Monday) + stopWeekday.CompareTo(DayOfWeek.Monday) + 1;
+        }
     }
 }
