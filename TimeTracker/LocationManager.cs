59b7fb4a6f36ec82831e3123b9902d15f9793746
Daniel Lohse <daniel.lenerd.lohse@gmail.com>
2015-06-23 22:48:16 +0200

    #23 LocationManager class added with basic method bodies

---
+++ TimeTracker/LocationManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Devices.Geolocation;
+
+namespace TimeTracker
+{
+
+    class LocationManager
+    {
+        uint _desireAccuracyInMetersValue = 10;
+        private Geolocator _geolocator;
+        public LocationManager()
+        {
+            _geolocator = new Geolocator();
+            
+        }
+
+        public async void GetCurrentLocation()
+        {
+            Geolocator geolocator = new Geolocator { DesiredAccuracyInMeters = _desireAccuracyInMetersValue };
+            //_geolocator.StatusChanged += OnStatusChanged;
+            Geoposition pos = await geolocator.GetGeopositionAsync();
+
+        }
+
+    }
+}
